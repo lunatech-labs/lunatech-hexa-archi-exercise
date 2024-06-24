2e1127cf92a7ee2d7ee4595639712af43d506960
SFE <sebastien.felix@lunatech.fr>
2024-06-24 07:46:56 +0200

    initial commit

---
+++ src/test/java/lunatech/TodoResourceTest.java
@@ -0,0 +1,21 @@
+package lunatech;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+class TodoResourceTest {
+    @Test
+    void testTodos() {
+        given()
+          .when().get("/api/todos")
+          .then()
+             .statusCode(200)
+             .body(is("[]"));
+    }
+
+}
